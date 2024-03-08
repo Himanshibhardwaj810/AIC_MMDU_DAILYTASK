0490eb053da453b0159ea0e908c8a3f2f26b075f
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-03-08 16:39:54 +0530

    String,Postfix and Prefix operations

---
+++ string.js
@@ -0,0 +1,32 @@
+let a="Hello"
+let b=" Himanshi"
+let c= a+b;
+console.log(c);
+
+console.log(1+"4");
+console.log("1"+4);
+console.log("1"+3+6);
+
+console.log(1+2+"3");//33
+
+console.log(+true);
+
+let d,e,f;
+d=e=f=4*4;
+console.log(e);
+
+let gamecounter=239;
+gamecounter++;
+console.log(gamecounter);
+
+let choice=90;
+++choice;
+console.log(choice);
+
+let x = 3;
+const y = ++x;
+console.log(x,y);
+
+let m = 3;
+const n = m++;
+console.log(m,n);
